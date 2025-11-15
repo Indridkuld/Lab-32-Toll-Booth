9a538d6de8b09f4d34136d07e155532a4c7ca7bc
Aidan <aidanmichaelwoodcock@gmail.com>
2025-11-14 21:38:01 -0800

    update

--- main.cpp
+++ main.cpp
@@ -16,15 +16,39 @@ void popDeque(deque<Car>& dq) ;
 int main() {
     deque<Car> tollLine; 
 
-    while (!tollLine.empty()) {
-        timeStep++;
-        
-        popDeque(tollLine);
+    popDeque(tollLine);
 
-
+    cout << "Initial Queue:\n";
+    for (Car& c : tollLine) {
+        cout << "    ";
+        c.print();
     }
 
+    while (!tollLine.empty()) {
+        timeStep++;
+        int randNum = rand() % 100; // random number between 0-99
 
+        if (randNum < 55) { // 55% head car pays and leaves
+            cout << "Time: " << timeStep << "Operation: Car paid: ";
+            tollLine.front().print();
+            tollLine.pop_front();
+        } else {
+            // 45% a new car joins
+            tollLine.push_back(Car());
+            cout << "Time: " << timeStep << " Operation: Joined lane: ";
+            tollLine.back().print();
+        }
+    }
+    // prints final state of the queue
+    cout << "\nQueue: " << endl;
+    if (tollLine.empty()) {
+            cout << "    Empty\n";
+        } else {
+            for (Car& c : tollLine) {
+                cout << "    ";
+                c.print();
+            }
+        }
     
     return 0; 
 }
